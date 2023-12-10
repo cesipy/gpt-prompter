95f5a600c41ad341a020cb4e8dac6c30ce012fc0
Cedric Sillaber <cedric.sillaber@gmail.com>
2023-12-10 16:21:11 +0100

    add tests

--- tests/tests_.py
+++ tests/tests_.py
@@ -10,9 +10,54 @@ sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
 
 import main
 
-def test_token_usage_fee():
+def test_token_usage_fee_1():
+    token_usage = [255, 347]
+    model = "gpt-4"
+
+    result = main.calculate_price(model, token_usage)
+    assert result == 2.847
+
+
+def test_token_usage_fee_2():
     token_usage = [1000, 1000]
     model = "gpt-4"
 
     result = main.calculate_price(model, token_usage)
     assert result == 9
+
+
+def test_token_usage_fee_3():
+    token_usage = [1000, 1000]
+    model = "gpt-3.5-turbo-1106"
+
+    result = main.calculate_price(model, token_usage)
+    assert result == 0.3
+
+
+def test_parse_args_default(monkeypatch):
+    # test with no command line arguments
+    monkeypatch.setattr('sys.argv', [''])
+    assert main.parse_args() == ("gpt-3.5-turbo-1106", "default")
+
+
+def test_parse_args_gpt3(monkeypatch):
+    # Test with --gpt3 argument
+    monkeypatch.setattr('sys.argv', ['', '--gpt3'])
+    assert main.parse_args() == ("gpt-3.5-turbo-1106", "default")
+
+
+def test_parse_args_gpt4_32k(monkeypatch):
+    # Test with --gpt4-32k argument
+    monkeypatch.setattr('sys.argv', ['', '--gpt4-32k'])
+    assert main.parse_args() == ("gpt-4-32k", "default")
+
+
+def test_parse_args_gpt4(monkeypatch):
+    # Test with --gpt4-32k argument
+    monkeypatch.setattr('sys.argv', ['', '--gpt4'])
+    assert main.parse_args() == ("gpt-4", "default")
+
+def test_parse_args_gpt4_followup(monkeypatch):
+    # Test with --gpt4-32k argument
+    monkeypatch.setattr('sys.argv', ['', '--gpt4', '--follow-up'])
+    assert main.parse_args() == ("gpt-4", "follow-up")
